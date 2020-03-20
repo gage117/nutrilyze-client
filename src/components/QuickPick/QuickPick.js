93ba9789b9a9a56c293abf758ee9a957f3bf2c82
gage117 <57643501+gage117@users.noreply.github.com>
2020-03-20 10:47:37 -0500

    Added alert for quick-pick ingredient add form to alert users when their input isn't a number or a fraction

--- src/components/QuickPick/QuickPick.js
+++ src/components/QuickPick/QuickPick.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './QuickPick.css'
 
-class QuickPick extends Component {
+function QuickPick(props) {
   /** JSX Generators */
-  generateIngredient(ingredient, index) {
+  function generateIngredient(ingredient, index) {
     return (
       <div key={index} className='list-div'>
         <div id={ingredient.name} className='qp__ingredient-bar qp__border-bottom'>
           <span>{ingredient.name}</span>
-          <button id={`${ingredient.name}-button`} className={`${ingredient.name} qp__add-button`} onClick={this.handleAddClick}>Add</button>
+          <button id={`${ingredient.name}-button`} className={`${ingredient.name} qp__add-button`} onClick={handleAddClick}>Add</button>
         </div>
         <div id={`${ingredient.name}-hidden`} className='qp__hidden expand-div qp__border-bottom'></div>
       </div>
     )
   }
   /** Meal functionality */
-  // generateMeal(meal, index) {
+  // function generateMeal(meal, index) {
   //   return (
   //     <div key={index} className='list-div'>
   //       <span>{meal.name}</span>
@@ -25,7 +25,7 @@ class QuickPick extends Component {
   //     </div>
   //   )
   // }
-  generateForm(item) {
+  function generateForm(item) {
     /** Multiple users functionality */
     /** Returns the checkbox and label JSX of a user */
     // function generateUserChoices(user) {
@@ -37,16 +37,16 @@ class QuickPick extends Component {
     //   )
     // }
     function getUnitOfMeasureByName(name) {
-      const item = this.getItemByName(name)
+      const item = getItemByName(name)
       return item.unit_of_measure
     }
     /** Multiple users, Apply-to Checkboxes JSX, add above serving size div */
     // <div className='apply-to-half'>
     //   <span className='apply-to-span'>Apply To: </span>
-    //   {this.props.state.users.map(user => generateUserChoices(user))}
+    //   {props.state.users.map(user => generateUserChoices(user))}
     // </div>
     return (
-      <form onSubmit={this.handleApplyClick} id={`${item}-apply`} className={`qp__add-form`}>
+      <form onSubmit={handleApplyClick} id={`${item}-apply`} className={`qp__add-form`}>
         <label className='qp__add-food-label'>
           <span className='block'>Serving:</span>
           <input type='text' id={`${item}-serving`} className='qp__serving-input' required />
@@ -58,16 +58,16 @@ class QuickPick extends Component {
   }
 
   /** gets entire item object by name */
-  getItemByName(name) {
+  function getItemByName(name) {
     let item;
-    this.props.state.ingredients.map(ingredient => {
+    props.state.ingredients.map(ingredient => {
       if (ingredient.name === name) {
         item = ingredient
       }
       return item
     })
     /** Meals functionality */
-    // this.props.state.meals.map(meal => {
+    // props.state.meals.map(meal => {
     //   if (meal.name === name) {
     //     item = meal
     //   }
@@ -79,9 +79,9 @@ class QuickPick extends Component {
   /**  */
 
   /** Button handlers */
-  handleApplyClick(event) {
+  function handleApplyClick(event) {
     event.preventDefault()
-    const item = this.getItemByName(event.target.getAttribute('id').split('-')[0])
+    const item = getItemByName(event.target.getAttribute('id').split('-')[0])
     
     let serving_size = item.serving_size;
     if (isNaN(serving_size)) {
@@ -89,38 +89,40 @@ class QuickPick extends Component {
       serving_size = parseFloat(serving_size[0] / serving_size[1]).toFixed(2)
     }
     const multiplier = parseFloat(event.target[`${item.name}-serving`].value / Number(serving_size)).toFixed(2)
-
-    console.log(multiplier)
     
-    const nutritionValues = {
-      calories: parseFloat(item.calories * multiplier).toFixed(0),
-      protein: parseFloat(item.protein * multiplier).toFixed(2),
-      carbs: parseFloat(item.carbs * multiplier).toFixed(2),
-      sugar: parseFloat(item.sugar * multiplier).toFixed(2),
-      fiber: parseFloat(item.fiber * multiplier).toFixed(2),
-      fat: parseFloat(item.fat * multiplier).toFixed(2),
-      sodium: parseFloat(item.sodium * multiplier).toFixed(2),
+    if (isNaN(multiplier)) {
+      alert('Serving size must be a number or a fraction (1/3)')
+    } else {
+      const nutritionValues = {
+        calories: parseFloat(item.calories * multiplier).toFixed(0),
+        protein: parseFloat(item.protein * multiplier).toFixed(2),
+        carbs: parseFloat(item.carbs * multiplier).toFixed(2),
+        sugar: parseFloat(item.sugar * multiplier).toFixed(2),
+        fiber: parseFloat(item.fiber * multiplier).toFixed(2),
+        fat: parseFloat(item.fat * multiplier).toFixed(2),
+        sodium: parseFloat(item.sodium * multiplier).toFixed(2),
+      }
+      
+      const usersToUpdate = [props.state.users[0]];
+      /** Multiple users functionality */
+      // for (let i = 0; i < props.state.users.length; i++) {
+      //   if (event.target[`${props.state.users[i].name}-list`].checked && !usersToUpdate.includes(props.state.users[i])) {
+      //     usersToUpdate.push(props.state.users[i])
+      //   }
+      // }
+  
+      props.handlers.updateUserNutrition(nutritionValues, usersToUpdate)
     }
-    
-    const usersToUpdate = [this.props.state.users[0]];
-    /** Multiple users functionality */
-    // for (let i = 0; i < this.props.state.users.length; i++) {
-    //   if (event.target[`${this.props.state.users[i].name}-list`].checked && !usersToUpdate.includes(this.props.state.users[i])) {
-    //     usersToUpdate.push(this.props.state.users[i])
-    //   }
-    // }
-
-    this.props.handlers.updateUserNutrition(nutritionValues, usersToUpdate)
   }
-  handleAddClick(event) {
+  function handleAddClick(event) {
     const item = event.target.getAttribute('id').split('-')[0]
-    const element = this.generateForm(item)
+    const element = generateForm(item)
     document.getElementById(`${item}-hidden`).classList.toggle('qp__hidden')
     document.getElementById(item).classList.toggle('qp__border-bottom')
     ReactDOM.render(element, document.getElementById(`${item}-hidden`))
   }
   /** Meal functionality */
-  // handleOrganizeMealClick() {
+  // function handleOrganizeMealClick() {
 
   // }
   /** Meal JSX, add to render above ingredients section */
@@ -136,21 +138,19 @@ class QuickPick extends Component {
   //       <span>Spaghetti Noodles, Pasta Sauce</span>
   //     </div>
   //   </div>
-  //   {this.props.state.meals.map((meal, index) => generateMeal(meal, index))}
+  //   {props.state.meals.map((meal, index) => generateMeal(meal, index))}
   // </section>
 
-  render() {
-    return (
-      <div className='quick-pick'>
-        <section className='qp__section qp__ingredients'>
-          <h2 className='qp__ingredients-header qp__h2'>Ingredients</h2>
-          <div id='qp__ingredients-container'>
-            {this.props.state.ingredients.map((ingredient, index) => this.generateIngredient(ingredient, index))}
-          </div>
-        </section>
-      </div>
-    )
-  }
+  return (
+    <div className='quick-pick'>
+      <section className='qp__section qp__ingredients'>
+        <h2 className='qp__ingredients-header qp__h2'>Ingredients</h2>
+        <div id='qp__ingredients-container'>
+          {props.state.ingredients.map((ingredient, index) => generateIngredient(ingredient, index))}
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default QuickPick
